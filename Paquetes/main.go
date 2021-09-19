c26b4d7338797ce214cf50f9d9d8b1cc6f55ec19
Oscar Lopez <oscareduardolp6@gmail.com>
2021-09-19 03:32:16 -0500

    Terminando el curso

--- Paquetes/main.go
+++ Paquetes/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"./greet"
 	"fmt"
+
+	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println("Hello")
-	greet.English()
+	quote.Hello()Experiencia con JavaScript (Vanilla, Node, React, Angular) , Typescript (Node y Deno) , ASP.NET, PHP, Java, Ionic Framework y Open Edge ABL
+	// Curso-Go.English()
+	// Curso-Go.English()
 }
